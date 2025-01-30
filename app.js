9bb153649d4c687e5e351c60e88b9b41ce971e5b
THAKURkamlesh <kamlesh@wegile.com>
2025-01-30 18:55:48 +0530

    Changed

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  nsole.log(`App listening at http://localhost:${port}`);
 });
