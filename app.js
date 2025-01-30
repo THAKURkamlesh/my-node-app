10a7fe2440b5268d5b74f6385e9e966d58db86b6
THAKURkamlesh <kamlesh@wegile.com>
2025-01-30 18:39:36 +0530

    Changed 'hello docker' to 'xyzz' in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Docker HUB server ');
+  res.send('Welcome to Docker HUB serverrrr ');
 });
 
 app.listen(port, () => {
