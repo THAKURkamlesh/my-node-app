c3d3ba5c79d0382c1a81ef5349f7d4048319e44f
THAKURkamlesh <kamlesh@wegile.com>
2025-01-31 12:26:41 +0530

    code pushed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Docker HUB serverrrr ');
+  res.send('Welcome to Docker HUB server are successfully deployed using jenkins pipeline ');
 });
 
 app.listen(port, () => {
