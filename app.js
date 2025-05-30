3b7cfc192b53cdb0a8956e90957d7d107dcecf4a
Wegile <wegile@wegiles-MacBook-Air.local>
2025-05-30 18:25:55 +0530

    new

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Docker HUB server are successfully deployed using jenkins pipeline xyz ');
+  res.send('Welcome to Docker HUB server are successfully deployed using jenkins pipeline xyzs ');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:$`);
 });
